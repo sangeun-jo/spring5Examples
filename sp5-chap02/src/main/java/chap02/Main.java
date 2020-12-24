ad017bd4d72e8614513c70fce130ed2ce610f499
sangeun-jo <dog8536@naver.com>
2020-12-24 12:29:16 +0900

    chap02 완료

---
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -0,0 +1,23 @@
+package chap02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(AppContext.class);
+		
+		Greeter g = ctx.getBean("greeter", Greeter.class);
+		String msg = g.greet("스프링");
+		System.out.println(msg);
+		
+		/*  싱글톤 확인 
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter", Greeter.class); 
+		System.out.println("(g1 == g2) = " + (g1 == g2)); 
+		*/ 
+		
+		ctx.close();
+	}
+}
